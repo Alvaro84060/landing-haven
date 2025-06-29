69dd8345022e4dce2fccc72b6c0cc0fe4dd33e39
Alvaro Arraras <85067108+Alvaro84060@users.noreply.github.com>
2025-06-29 17:29:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+// Fade in on scroll
+function revealElements() {
+  const reveals = document.querySelectorAll('.reveal');
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealElements);
+window.addEventListener('load', revealElements);
